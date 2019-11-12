aefb374e591e9cb5e79d794896da7dd06ec16f79
danielsky <karma0rg4u@gmail.com>
2019-11-12 10:59:03 +0000

    1.12*: anecdotes step1

--- src/index.js
+++ src/index.js
@@ -1,100 +1,30 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = ({header}) => (
-    <h1>{header}</h1>
-)
-
 const Button = ({onClick, text}) => (
     <button onClick={onClick}>{text}</button>
 )
 
-const Statistic = ({text, value}) => {
-    if (text === 'positive') {
-        return (
-            <tr>
-                <td>
-                    <p>{text}</p>
-                </td>
-                <td>
-                    <p>{value} %</p>
-                </td>
-            </tr>
-        )
-    }   else {
-        return (
-            <tr>
-                <td>
-                    <p>{text}</p>
-                </td>
-                <td>
-                    <p>{value}</p>
-                </td>
-            </tr>
-        )
-    }
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const generateRandom = value => setSelected(value)
+    return (
+        <div>
+            <p>{props.anecdotes[selected]}</p>
+            <Button onClick={() => generateRandom(Math.floor(Math.random()*anecdotes.length))} text='next anecdote'/>
+        </div>
+    )
 }
 
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-const Statistics = (props) => {
-    if (props.value1 > 0 || props.value2 > 0 || props.value3 > 0) {
-        return (
-            <div>
-                <table>
-                    <tbody>
-                        <Statistic text='good' value={props.value1}/>
-                        <Statistic text='neutral' value={props.value2}/>
-                        <Statistic text='bad' value={props.value3}/>
-                        <Statistic text='all' value={props.value4}/>
-                        <Statistic text='average' value={props.value5}/>
-                        <Statistic text='positive' value={props.value6}/>
-                    </tbody>
-                </table>
-            </div>
-        )
-    } else {
-        return (
-            <p>No feedback given</p>
-        )
-    }
-}
-
-const App = () => {
-    // save clicks of each button to own state
-    const header1 = 'give feedback'
-    const header2 = 'statistics'
-    const text1 = 'good'
-    const text2 = 'neutral'
-    const text3 = 'bad'
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-
-    const handleGoodClick = (value) => setGood(value)
-    const handleNeutralClick = (value) => setNeutral(value)
-    const handleBadClick = (value) => setBad(value)
-    const all = good + neutral + bad
-    const average = ((good - bad) / all).toFixed(1)
-    const positive = ((good / all) * 100).toFixed(1)
-
-  return (
-    <div>
-        <Header header={header1}/>
-        <Button onClick={() => handleGoodClick(good + 1)} text={text1}/>
-        <Button onClick={() => handleNeutralClick(neutral + 1)} text={text2}/>
-        <Button onClick={() => handleBadClick(bad + 1)} text={text3}/>
-        <Header header={header2}/>
-        <Statistics 
-            value1={good}
-            value2={neutral}
-            value3={bad} 
-            value4={all}
-            value5={average}
-            value6={positive}/>
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, 
-    document.getElementById('root')
+ReactDOM.render(
+    <App anecdotes={anecdotes} />, document.getElementById('root')
 )
